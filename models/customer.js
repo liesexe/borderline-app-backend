92f5b8af83720b0388478e34325cd713e94b967d
Fernando Pérez <fernando.javier97@hotmail.com>
2024-12-20 02:07:18 -0500

    feat: changed customer entity name

--- models/customer.js
+++ models/customer.js
@@ -40,5 +40,5 @@ const customerSchema = new mongoose.Schema({
   }
 });
 
-const Customer = mongoose.model('Customer', customerSchema);
-module.exports = Customer;
+const customer = mongoose.model('Customer', customerSchema);
+module.exports = customer;
